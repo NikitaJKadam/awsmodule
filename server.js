86636fbb06d49f1aaa8f4cefb169156a5fdce469
Nikita Kadam <121048089+NikitaJKadam@users.noreply.github.com>
2022-12-30 19:03:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-const express=require('express')
-var app=express()
-
-app.listen(5000)
+const express=require('express');
+var app=express();
+app.get("/",function(req,resp)=>{
+        resp.sendFile("public/first.html",{root:__dirname})
+        });
+app.listen(5000);
